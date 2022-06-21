c603ea3640b59e6f75169bdaf13aef11396f2faf
Alpatexx <107890239+Alpatexx@users.noreply.github.com>
2022-06-21 12:18:46 +0200

    Delete mal-fest-intranet.com/public_html directory

--- mal-fest-intranet.com/public_html/supprimer_groupe.php
+++
@@ -1,9 +0,0 @@
-<?php
-$groupes = json_decode(file_get_contents("groupes.json"), true);
-foreach($groupes as $key => $groupe) {
-    if($key == $_GET['groupe']){
-        unset($groupes[$key]);
-    }
-}
-file_put_contents("groupes.json", json_encode($groupes));
-header("Location: accueil.php");
--- mal-fest-intranet.com/public_html/supprimer_personnage.php
+++
@@ -1,9 +0,0 @@
-<?php
-$personnages = json_decode(file_get_contents("users.json"), true);
-foreach($personnages as $key => $personnage) {
-    if($personnage['id'] == $_GET['id']) {
-        unset($personnages[$key]);
-    }
-}
-file_put_contents("users.json", json_encode($personnages));
-header("Location: accueil.php");
